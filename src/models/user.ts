446f1a778da9fcb6f85361e0095681ebe41fcea6
Ali Abrorov <ahrorabrorov26@gmail.com>
2024-05-16 09:36:07 +0500

    first commit

---
+++ src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  auth0Id: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    trim: true,
+  },
+  addressLine1: {
+    type: String,
+    trim: true,
+  },
+  city: {
+    type: String,
+    trim: true,
+  },
+  country: {
+    type: String,
+    trim: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
